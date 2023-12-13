0c956091b838513e5f073a60afe2f12d569df8a5
Macbook Pro 2017 <>
2023-12-13 21:31:55 +0700

    test products on prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
+import { getProducts } from "@/libs/getProducts";
+
+export default async function Home() {
+  const data = await getProducts();
+
+  console.log({ data });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello
+      <p>{JSON.stringify(data)}</p>
+    </main>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="w-full h-56 border-t border-slate-200 py-4">
+      <div className="container mx-auto">Copyright 2023</div>
+    </footer>
+  );
+}
