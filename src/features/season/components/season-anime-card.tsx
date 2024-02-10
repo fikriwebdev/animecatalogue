798f0deec046534d206d8a8acade4b28bf6bcc2b
Macbook Pro 2017 <>
2024-02-11 00:03:48 +0700

    fix: line-clamp not work on synopsis

--- src/features/season/components/season-anime-card.tsx
+++ src/features/season/components/season-anime-card.tsx
@@ -28,12 +28,12 @@ export default function SeasonAnimeCard({
         <div className="absolute w-full h-full bg-gradient-to-b from-transparent  to-black/100 bottom-0 z-[2] p-2"></div>
         <div className="absolute bottom-4 p-2 w-full z-[3]">
           <div className="mb-auto">
-            <h1 className="text-base md:text-lg line-clamp-1 md:line-clamp-none font-semibold">
+            <h1 className="text-base md:text-lg line-clamp-1 md:line-clamp-2 font-semibold">
               {title}
             </h1>
-            <p className="hidden md:block line-clamp-2 md:line-clamp-3 mt-2 text-sm font-light">
-              {synopsis}
-            </p>
+            <div className="hidden md:block ">
+              <p className="line-clamp-3 mt-2 text-sm font-light">{synopsis}</p>
+            </div>
             <div className="flex-col md:flex-row items-center gap-2 text-sm font-light mt-2">
               <div className="flex items-center gap-2">
                 <Calendar className="w-4 h-4" />
