8245a84aaefa6c49d9a250e5d0d0bcdb8ec79ee6
Macbook Pro 2017 <>
2024-02-17 00:28:36 +0700

    add: parallel streaming to increase load speed on homepage

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,49 +1,87 @@
 import AnimeCarousel from "@/components/anime-carousel";
+import { AnimeSkeletonGrid } from "@/components/anime-skeleton-grid-list";
 import { getTopAnime } from "@/libs/get-top-anime";
 import { getUpcomingAnime } from "@/libs/get-upcoming-anime";
+import { Suspense } from "react";
 
-export default async function ViewHome() {
-  const [
-    upcomingAnime,
-    topAnime,
-    topAiringAnime,
-    mostPopularAnime,
-    mostFavoritAnime,
-  ] = await Promise.all([
-    getUpcomingAnime(),
-    getTopAnime(),
-    getTopAnime("airing"),
-    getTopAnime("bypopularity"),
-    getTopAnime("favorite"),
-  ]);
+async function UpcomingAnimeCarousel() {
+  const upcomingAnime = await getUpcomingAnime();
 
   return (
+    <AnimeCarousel
+      anime={{ animes: upcomingAnime.anime }}
+      title={upcomingAnime.title}
+    />
+  );
+}
+
+async function TopAnimeCarousel() {
+  const topAnime = await getTopAnime();
+
+  return (
+    <AnimeCarousel
+      anime={topAnime}
+      title="Top Anime"
+      withRanking
+      viewAllHref="/top-anime"
+    />
+  );
+}
+
+async function TopAiringAnimeCarousel() {
+  const topAiringAnime = await getTopAnime("airing");
+
+  return (
+    <AnimeCarousel
+      anime={topAiringAnime}
+      title="Top Airing Anime"
+      viewAllHref="/top-airing"
+    />
+  );
+}
+
+async function PopularAnimeCarousel() {
+  const popularAnime = await getTopAnime("bypopularity");
+
+  return (
+    <AnimeCarousel
+      anime={popularAnime}
+      title="Most Popular Anime"
+      viewAllHref="/popular"
+    />
+  );
+}
+
+async function FavoriteAnime() {
+  const favoriteAnime = await getTopAnime("favorite");
+
+  return (
+    <AnimeCarousel
+      anime={favoriteAnime}
+      title="Most Favorite Anime"
+      viewAllHref="/favorite"
+    />
+  );
+}
+
+export default function ViewHome() {
+  return (
     <>
-      <AnimeCarousel
-        anime={{ animes: upcomingAnime.anime }}
-        title={upcomingAnime.title}
-      />
-      <AnimeCarousel
-        anime={topAnime}
-        title="Top Anime"
-        withRanking
-        viewAllHref="/top-anime"
-      />
-      <AnimeCarousel
-        anime={topAiringAnime}
-        title="Top Airing Anime"
-        viewAllHref="/top-airing"
-      />
-      <AnimeCarousel
-        anime={mostPopularAnime}
-        title="Most Popular Anime"
-        viewAllHref="/popular"
-      />
-      <AnimeCarousel
-        anime={mostFavoritAnime}
-        title="Most Favorite Anime"
-        viewAllHref="/favorite"
-      />
+      <Suspense fallback={<AnimeSkeletonGrid />}>
+        <UpcomingAnimeCarousel />
+      </Suspense>
+      <Suspense fallback={<AnimeSkeletonGrid />}>
+        <TopAnimeCarousel />
+      </Suspense>
+      <Suspense fallback={<AnimeSkeletonGrid />}>
+        <TopAiringAnimeCarousel />
+      </Suspense>
+      <Suspense fallback={<AnimeSkeletonGrid />}>
+        <PopularAnimeCarousel />
+      </Suspense>
+      <Suspense fallback={<AnimeSkeletonGrid />}>
+        <FavoriteAnime />
+      </Suspense>
     </>
   );
 }
